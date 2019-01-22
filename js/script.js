bf195fc7bcc0edda338e1d3a25fdfc71188caa84
Asmaa176 <43960688+Asmaa176@users.noreply.github.com>
2019-01-22 19:55:13 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+﻿
+$(document).ready(function(){
+
+    $('.menu').click(function(){
+        $('.list').slideToggle();
+    });
+
+     $(window).resize(function(){
+        if($(window).width() > 767) {
+            $('.list').toggleClass('responsive-nav').css('display','');
+        }
+    });  
+
+});
